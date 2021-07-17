fad0c7276e115b6ea93d9d5f059b412dc87595cf
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-17 23:13:37 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,6 @@
-if game.PlaceId ~= (placeid here) then ...
+if game.PlaceId == 5827078246 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/CoinsHero/main/coins%20sim.lua", true))()
+end
+if game.PlaceId == 7029146093 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/nekosarecute.lua", true))()
+end
