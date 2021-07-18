0d23608f67b3f91edbdb65a4471d5227c347c68b
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:52:13 +0800

    Update whitelistload.lua

--- whitelistload.lua
+++ whitelistload.lua
@@ -1,6 +1,5 @@
-if game.Players.LocalPlayer.UserId == 1644413979 or 451701301 or 1598126809 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/load.lua", true))()
-    else
-    game.Players.LocalPlayer:Kick("amogus")
-    print("Denied")
-end
+Players.PlayerAdded:Connect(function(Player)
+   if (WhitelistedIDs[Player.UserId]) then
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/load.lua", true))()
+   end
+end)
