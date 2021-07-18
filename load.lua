809d7c82f6f718b61dbb290bb6f8fe9903a4d5ee
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:42:09 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,3 +1,11 @@
+if game.Players.LocalPlayer.UserId == 1644413979 or 451701301 or 1598126809 then
+    print("Access Enabled")
+    else
+    game.Players.LocalPlayer:Kick("amogus")
+    print("Denied")
+end
+
+
 if game.PlaceId == 5827078246 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/CoinsHero/main/coins%20sim.lua", true))()
 end
@@ -8,5 +16,5 @@ if game.PlaceId == 3295514368 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/sandhurst.lua", true))()
 end
 if game.PlaceId == 6573836917 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/pekorausada.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/pekorausada.lua", true))()
 end
