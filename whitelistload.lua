5ec75e6c1f72866e9b369f4583ce63c980aecba0
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:47:45 +0800

    Update whitelistload.lua

--- whitelistload.lua
+++ whitelistload.lua
@@ -1,5 +1,5 @@
 if game.Players.LocalPlayer.UserId == 1644413979 or 451701301 or 1598126809 then
-    print("Access Enabled")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/load.lua", true))()
     else
     game.Players.LocalPlayer:Kick("amogus")
     print("Denied")
