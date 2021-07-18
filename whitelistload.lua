c019bb6ed57b8a0aae082852e881d338fd228259
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:52:42 +0800

    Update whitelistload.lua

--- whitelistload.lua
+++ whitelistload.lua
@@ -1,3 +1,12 @@
+local Players   = game:GetService("Players");
+local LPlayer   = Players.LocalPlayer;
+local LPlayerID = LPlayer.UserId;
+local WhitelistedIDs = {
+   [1644413979] = true;
+   [451701301] = true;
+   [1598126809] = true;
+}
+
 Players.PlayerAdded:Connect(function(Player)
    if (WhitelistedIDs[Player.UserId]) then
        loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/load.lua", true))()
