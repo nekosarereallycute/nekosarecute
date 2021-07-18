0b0219544f8cb1415fbfba3e6cce5a8937f67ab8
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:47:16 +0800

    Add files via upload

---
+++ whitelistload.lua
@@ -0,0 +1,6 @@
+if game.Players.LocalPlayer.UserId == 1644413979 or 451701301 or 1598126809 then
+    print("Access Enabled")
+    else
+    game.Players.LocalPlayer:Kick("amogus")
+    print("Denied")
+end
