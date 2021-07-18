11b2b5ae158b728f8793d647956b485915133123
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-07-18 21:44:58 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,11 +1,3 @@
-if game.Players.LocalPlayer.UserId == 1644413979 or 451701301 or 1598126809 then
-    print("Access Enabled")
-    else
-    game.Players.LocalPlayer:Kick("amogus")
-    print("Denied")
-end
-
-
 if game.PlaceId == 5827078246 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/CoinsHero/main/coins%20sim.lua", true))()
 end
