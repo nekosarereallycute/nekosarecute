edd9ae6b5b0976ebb18ae6d2a603eacc1d239f05
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-08-20 09:09:00 +0800

    Update uwuuwu.lua

--- uwuuwu.lua
+++ uwuuwu.lua
@@ -1,1 +1,36 @@
+if game.PlaceId == 5827078246 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/CoinsHero/main/coins%20sim.lua", true))()
+elseif
+game.PlaceId == 7029146093 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/nekosarecute.lua", true))()
+elseif
+game.PlaceId == 3295514368 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/sandhurst.lua", true))()
+elseif
+game.PlaceId == 6573836917 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/pekorausada.lua", true))()
+elseif
+game.PlaceId == 4586420508 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/speed%20sim.lua", true))()
+elseif
+game.PlaceId == 6265485611 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/infinity%20power%20sim.lua", true))()
+elseif
+game.PlaceId == 6808416928 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/aimblox.lua", true))()
+elseif
+game.PlaceId == 5475935012 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/cart%20ride.lua", true))()
+elseif
+game.PlaceId == 6897226634 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/timber.lua", true))()
+elseif
+game.PlaceId == 662417684 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/luckyblock.lua", true))()
+elseif
+game.PlaceId == 5188367351 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/ftnekouwu.lua", true))()
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/tps.lua", true))()
+end
 
