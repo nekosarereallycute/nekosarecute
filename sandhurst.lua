404c40b45942acba28f050c006f0f8c8a5811cfc
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-09-11 17:34:32 +0800

    Update sandhurst.lua

--- sandhurst.lua
+++ sandhurst.lua
@@ -124,6 +124,19 @@ local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercont
 ValiantAimHacks["TeamCheck"] = true
 setreadonly(mt, false)
 
+mt.__index = newcclosure(function(t, k)
+    if t:IsA("Mouse") and (k == "Hit" or k == "Target") then
+        if ValiantAimHacks.checkSilentAim() then
+            local CPlayer = rawget(ValiantAimHacks, "Selected")
+            if CPlayer and CPlayer.Character and CPlayer.Character.FindFirstChild(CPlayer.Character, "Head") then
+                return (k == "Hit" and CPlayer.Character.Head.CFrame or CPlayer.Character.Head)
+            end
+        end
+    end
+    return backupindex(t, k)
+end)
+setreadonly(mt, true)
+
 
 function silentaim()
     spawn(function()
@@ -135,19 +148,9 @@ function silentaim()
                 ValiantAimHacks["SilentAimEnabled"] = true
             end
         end
-        mt.__index = newcclosure(function(t, k)
-            if t:IsA("Mouse") and (k == "Hit" or k == "Target") then
-                if ValiantAimHacks.checkSilentAim() then
-                    local CPlayer = rawget(ValiantAimHacks, "Selected")
-                    if CPlayer and CPlayer.Character and CPlayer.Character.FindFirstChild(CPlayer.Character, "Head") then
-                        return (k == "Hit" and CPlayer.Character.Head.CFrame or CPlayer.Character.Head)
-                    end
-                end
-            end
-            return backupindex(t, k)
-        end)
     end)
 end
+
 
 
 
