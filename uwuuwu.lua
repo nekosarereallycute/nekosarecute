3c67237d0c4bd7a6d1694bdf65e4552af5bc645f
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-09-12 15:27:35 +0800

    Update uwuuwu.lua

--- uwuuwu.lua
+++ uwuuwu.lua
@@ -30,7 +30,16 @@ game.PlaceId == 662417684 then
 elseif
 game.PlaceId == 5188367351 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/ftnekouwu.lua", true))()
+elseif
+game.PlaceId == 2788229376 then
+   loadstring(game:HttpGet("loadstringhttps://raw.githubusercontent.com/nekomancer69420/timeless/main/dahoodobftime.lua", true))()
+elseif
+game.PlaceId == 6490016198 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/timeless/main/slayertyccoonreal.lua", true))()
+elseif
+game.PlaceId == 606849621 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/jailbreakobfuscated.lua", true))()
 else
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/tps.lua", true))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/timeless/main/nice.lua", true))()
 end
 
