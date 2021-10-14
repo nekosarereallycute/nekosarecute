d6b3903c1e67d3f222c5be75c6c355b185a29281
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-10-14 22:43:09 +0800

    Create uwuuwu.lua

--- uwuuwu.lua
+++ uwuuwu.lua
@@ -1,1 +1,13 @@
-game.Players.LocalPlayer:Kick("auth error, are you trying to bypass this?")
+local function notify(title,text,dur)
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = title,
+            Text = text,
+            Duration = dur or 5
+        })
+    end
+
+notify("verifying", "Please wait..")
+
+wait(5)
+
+game.Players.LocalPlayer:Kick("noob amogus error code 96969420")
