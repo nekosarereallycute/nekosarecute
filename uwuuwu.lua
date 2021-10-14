4952e1eea39f119bf79d88a6fb7deb5e858e0096
nekomancer69420 <87574009+nekomancer69420@users.noreply.github.com>
2021-10-14 22:34:32 +0800

    Update uwuuwu.lua

--- uwuuwu.lua
+++ uwuuwu.lua
@@ -1,3 +1,1 @@
-if game.PlaceId == 6951762614 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nekomancer69420/nekosarecute/main/ufo%20sim.lua", true))()
-end
+game.Players.LocalPlayer:Kick("auth error, are you trying to bypass this?")
